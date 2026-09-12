5d8d24e63649fc4793a6bbf853f1cb216b7157f5
Kasun <kasun.kanakarathne@unicast.ne.jp>
2026-09-12 13:34:19 +0900

    feat: 盤面の境界フィルタリングを備えたgetValidMoves関数を追加

--- knightMoves.js
+++ knightMoves.js
@@ -1,1 +1,16 @@
-console.log("knightMoves.js is running!");
+function getValidMoves([x, y]) {
+  const offsets = [
+    [2, 1], [2, -1], [-2, 1], [-2, -1],
+    [1, 2], [1, -2], [-1, 2], [-1, -2],
+  ];
+
+  const moves = offsets.map(([dx, dy]) => [x + dx, y + dy]);
+
+  return moves.filter(
+    ([newX, newY]) => newX >= 0 && newX <= 7 && newY >= 0 && newY <= 7
+  );
+}
+
+console.log(getValidMoves([0, 0]));
+console.log(getValidMoves([3, 3]));
+console.log(getValidMoves([7, 7]));
