cb7f40aa352bf345c86dcfff3dd2b189b787ec18
Kasun <kasun.kanakarathne@unicast.ne.jp>
2026-09-12 13:36:07 +0900

    feat: サイクルを防ぐためのvisited管理を備えたBFSアルゴリズムを追加

--- knightMoves.js
+++ knightMoves.js
@@ -10,6 +10,35 @@ function getValidMoves([x, y]) {
     ([newX, newY]) => newX >= 0 && newX <= 7 && newY >= 0 && newY <= 7
   );
 }
+function bfs(start, end) {
+  const queue = [[start]]; // each queue entry is a full path (array of squares)
+  const visited = new Set();
+  visited.add(start.toString());
+
+  while (queue.length > 0) {
+    const path = queue.shift();
+    const currentSquare = path[path.length - 1];
+
+    if (currentSquare[0] === end[0] && currentSquare[1] === end[1]) {
+      return path;
+    }
+
+    const nextMoves = getValidMoves(currentSquare);
+
+    for (const move of nextMoves) {
+      const key = move.toString();
+      if (!visited.has(key)) {
+        visited.add(key);
+        queue.push([...path, move]);
+      }
+    }
+  }
+
+  return null; // shouldn't happen on a valid 8x8 board
+}
+
+console.log(bfs([0, 0], [1, 2]));
+console.log(bfs([3, 3], [4, 3]));
 
 console.log(getValidMoves([0, 0]));
 console.log(getValidMoves([3, 3]));
